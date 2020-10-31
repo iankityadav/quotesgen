95b5c402e1cfb9490bd60507a94272ff9a626fdc
Ankit Yadav <64765952+iankityadav@users.noreply.github.com>
2020-10-31 18:47:45 +0530

    Update main.js
    Changing links attributes with change of quotes for social sharing of quotes.

--- main.js
+++ main.js
@@ -8,7 +8,8 @@ $.ajax({
     $(document).ready(function(){
         $("#a").text(res.content);
         $("#b").text("- "+res.author);
-        
+        $("#fb-share").attr('href','https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fiankityadav.github.io%2Fquotesgen&quote='+res.content+'%0A-%20'+res.author+'%0A%0Aiankityadav.github.io%2Fquotesgen%2F');
+        $("#tweet").attr('href','https://twitter.com/intent/tweet?source=http%3A%2F%2Fiankityadav.github.io%2Fquotesgen&text='+res.content+'%0A-%20'+res.author+'%0A%0Aiankityadav.github.io%2Fquotesgen%2F');
     });
   }
 });
